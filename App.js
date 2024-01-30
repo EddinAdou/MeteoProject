2e2ea25c7a9aa9c72516a4ca4ca46af997a1c288
EddinAdou <142503187+EddinAdou@users.noreply.github.com>
2024-01-30 22:34:24 +0100

    Step 2

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null)
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location is required!');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location);
+    })();
+  }, []);
+  let text= 'Waiting..'
+  if (errorMsg) {
+    text= errorMsg
+  }else{
+    text = JSON.stringify(location)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Eddin</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>{text}</Text>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
