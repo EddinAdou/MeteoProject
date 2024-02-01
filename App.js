94cf286eba7704ddd75000ce0fd1f979a000f1ec
EddinAdou <142503187+EddinAdou@users.noreply.github.com>
2024-02-01 10:03:45 +0100

    Step 5/1

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Platform, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Platform, Image, ScrollView, ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
 import Axios from 'axios';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
-  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [weatherData, setWeatherData] = useState(null);
   const [forecastData, setForecastData] = useState([]);
@@ -26,59 +27,68 @@ export default function App() {
         setWeatherData(response.data);
         const forecastResponse = await Axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=02f3b877027f57eca803d14bdf6a603f&units=metric&lang=fr`);
         setForecastData(forecastResponse.data.list);
+        setLoading(false);
       } catch (error) {
         setErrorMsg('Error fetching weather data');
+        setLoading(false);
       }
     })();
   }, []);
 
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={['rgba(9,29,85,1)', 'rgba(0,0,0,1)']}
+      style={styles.container}
+    >
       <Text style={styles.title}>Ma position</Text>
       {errorMsg && <Text style={styles.error}>{errorMsg}</Text>}
+      {loading && (
+        <ActivityIndicator size="large" color="#00ff00"/>
+      )}
+      
       {weatherData && (
         <View style={styles.weatherContainer}>
           <Text style={styles.city}>{weatherData.name}</Text>
           <Text style={styles.temp}>{Math.round(weatherData.main.temp)}°C</Text>
           <Text style={styles.description}>{weatherData.weather[0].description}</Text>
           <Image
-            source={{ uri: `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png` }}
+            source={{ uri: `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png` }}
             style={styles.weatherIcon}
             resizeMode="contain"
           />
         </View>
       )}
+
       <Text style={styles.forecastTitle}>Prévisions sur 5 jours</Text>
       <ScrollView vertical={true} contentContainerStyle={styles.forecastContainer}>
-  {forecastData.map((forecast, index) => (
-    <View key={index} style={styles.forecastItem}>
-      <Text style={styles.forecastTime}>
-  {new Date(forecast.dt * 1000).toLocaleDateString('fr-FR', {weekday: 'long'}).charAt(0).toUpperCase() +
-  new Date(forecast.dt * 1000).toLocaleDateString('fr-FR', {weekday: 'long'}).slice(1)},{' '}
-  {new Date(forecast.dt * 1000).toLocaleTimeString('fr-FR', {hour: 'numeric'})}
-</Text>
+        {forecastData.map((forecast, index) => (
+          <View key={index} style={styles.forecastItem}>
+            <Text style={styles.forecastTime}>
+              {new Date(forecast.dt * 1000).toLocaleDateString('fr-FR', { weekday: 'long' })
+                .slice(0,1).toUpperCase() }{''}
+              {new Date(forecast.dt * 1000).toLocaleDateString('fr-FR', { weekday: 'long' })
+                .slice(1,3)}{' '}
+              {new Date(forecast.dt * 1000).toLocaleTimeString('fr-FR', { hour: 'numeric' })}
+            </Text>
 
-
-      <Image
-        source={{ uri: `http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png` }}
-        style={styles.forecastIcon}
-        resizeMode="contain"
-      />
-      <Text style={styles.forecastTemp}>{Math.round(forecast.main.temp)}°C</Text>
-    </View>
-  ))}
-</ScrollView>
-
-
+            <Image
+              source={{ uri: `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png` }}
+              style={styles.forecastIcon}
+              resizeMode="contain"
+            />
+            <Text style={styles.forecastTemp}>{Math.round(forecast.main.temp)}°C</Text>
+          </View>
+        ))}
+      </ScrollView>
+      
       <StatusBar style="auto" />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e2e6e1',
     alignItems: 'center',
     justifyContent: 'center',
     paddingHorizontal: 20,
@@ -88,6 +98,8 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
     marginTop: 20,
+    color: 'white',
+    fontFamily: 'Georgia',
   },
   error: {
     color: 'red',
@@ -100,46 +112,49 @@ const styles = StyleSheet.create({
     fontSize: 80,
     marginBottom: 10,
     fontWeight: 'bold',
-    color:'#54565b',
+    color:'#fff',
   },
   weatherIcon: {
-    width: 150,
-    height: 150,
+    width: 100,
+    height: 100,
     marginVertical: 10,
   },
   city:{
     fontSize: 27,
     fontWeight: "500",
-    color: '#54565b'
+    color: '#fffe'
   },
   description:{
     fontSize: 25,
     lineHeight: 35,
     textAlign: 'center',
-    color:'#7a7c7f',
+    color:'#7a7c9f',
     fontWeight: 'bold',
   },
   forecastTitle: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginTop: 20,
+    marginTop: 2,
+    color: '#f0f8ff',
   },
   forecastContainer: {
     marginTop: 10,
-    paddingHorizontal: 20,
+    paddingHorizontal: 40,
   },
   forecastItem: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 10,
+    marginTop: 0,
+    marginBottom: 9,
     backgroundColor: '#fff',
-    padding: 10,
+    padding: 30,
     borderRadius: 8,
     elevation: 2,
   },
   forecastTime: {
     fontSize: 16,
     marginRight: 10,
+    fontWeight: 'bold',
   },
   forecastIcon: {
     width: 50,
@@ -148,7 +163,7 @@ const styles = StyleSheet.create({
   },
   forecastTemp: {
     fontSize: 16,
-  },
-  
-});
+    fontWeight: 'bold',
 
+  },
+});
